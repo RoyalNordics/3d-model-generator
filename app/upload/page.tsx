a3e7317c6496df251ad00a6906bdfae81d4c4dfa
Thomas Flyttov Møller <thomas.flyttov@hotmail.com>
2025-03-30 15:32:37 +0000

    ✅ Create basic upload page

---
+++ app/upload/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UploadPage = () => {
+  return (
+    <div>
+      <h1>Create New Project</h1>
+      <form>
+        {/* Add form fields here */}
+        <button type="submit">Create Project</button>
+      </form>
+    </div>
+  );
+};
+
+export default UploadPage;
